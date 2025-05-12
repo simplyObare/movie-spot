5fb6aa96876dc36901c1ac42ae0aefbec6e32aaa
simplyObare <josephatowiny@gmail.com>
2025-05-12 19:13:35 +0300

    – Making GET Requests to the TMDB API to Fetch Movie Data and Passing it to the Template – Looping Through Movies  – Displaying Movie Posters

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.conf import settings
+import requests
 
 
 def landing_page(request):
     category = request.GET.get("category", "popular")
 
     API_KEY = settings.TMDB_API_KEY
-    return render(request, "movies/landing_page.html")
+    base_url = "https://api.themoviedb.org/3/movie/"
+    url = f"{base_url}{category}?api_key={API_KEY}&language=en-US&page=1"
+    response = requests.get(url)
+    data = response.json().get("results", [])
+    print(data)
+
+    return render(
+        request, "movies/landing_page.html", {"movies": data, "category": category}
+    )
