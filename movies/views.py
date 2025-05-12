09ab17e04a1815b89f182d1e4bda96004e2a38ec
simplyObare <josephatowiny@gmail.com>
2025-05-12 17:28:32 +0300

    Building the Categories Form

--- movies/views.py
+++ movies/views.py
@@ -3,5 +3,7 @@ from django.conf import settings
 
 
 def landing_page(request):
+    category = request.GET.get("category", "popular")
+
     API_KEY = settings.TMDB_API_KEY
     return render(request, "movies/landing_page.html")
