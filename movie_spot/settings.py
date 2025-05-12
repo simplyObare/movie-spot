00f843ae20142ade6c8443214bbc4fee8215f656
simplyObare <josephatowiny@gmail.com>
2025-05-12 15:25:59 +0300

    Using Python-dotenv & .env: Keeping Our API Key Safe and Accessible

--- movie_spot/settings.py
+++ movie_spot/settings.py
@@ -1,4 +1,9 @@
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def landing_page(request):
+    API_KEY = settings.TMDB_API_KEY
     return render(request, "movies/landing_page.html")
