5c98e004ab9f926a691d0ebc7cae26bb3c49a16a
simplyObare <josephatowiny@gmail.com>
2025-05-13 14:02:01 +0300

    add Infinite Scroll with HTMX

--- movies/views.py
+++ movies/views.py
@@ -6,23 +6,28 @@ import requests
 def landing_page(request):
     category = request.GET.get("category", "popular")
     search_query = request.GET.get("search", "")
+    page = int(request.GET.get("page", 1))
+    next_page = page + 1
 
     API_KEY = settings.TMDB_API_KEY
     base_url = "https://api.themoviedb.org/3/movie/"
     error_message = ""
 
     if search_query:
-        url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&language=en-US&query={search_query}&page=1&include_adult=false"
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={API_KEY}&language=en-US&query={search_query}&page={page}&include_adult=false"
     else:
-        url = f"{base_url}{category}?api_key={API_KEY}&language=en-US&page=1"
+        url = f"{base_url}{category}?api_key={API_KEY}&language=en-US&page={page}"
     try:
         response = requests.get(url)
         data = response.json().get("results", [])
         print(data)
+        total_pages = response.json().get("total_pages", 1)
+        has_next_page = page < total_pages
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data from TMDB API: {e}")
         data = []
         error_message = "Error fetching data from TMDB API. Please try again later."
+        has_next_page = False
 
     if request.headers.get("HX-Request"):
         return render(
@@ -33,6 +38,8 @@ def landing_page(request):
                 "category": category,
                 "search_query": search_query,
                 "error_message": error_message,
+                "next_page": next_page,
+                "has_next_page": has_next_page,
             },
         )
 
@@ -44,5 +51,7 @@ def landing_page(request):
             "category": category,
             "search_query": search_query,
             "error_message": error_message,
+            "next_page": next_page,
+            "has_next_page": has_next_page,
         },
     )
