e64dc1ef5128687e57e886dbff8883752fead535
simplyObare <josephatowiny@gmail.com>
2025-05-13 22:17:47 +0300

    creating registration form and Routing  to Registration Page

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Login
+from .views import Login, RegisterUser
 
 
 urlpatterns = [
     path("login/", Login.as_view(), name="login"),
+    path("register/", RegisterUser.as_view(), name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
+from .forms import RegisterUserForm
+from django.contrib.auth import login
 
 
 class Login(LoginView):
     template_name = "users/accounts/login.html"
+
+
+class RegisterUser(FormView):
+    template_name = "users/accounts/register.html"
+    form_class = RegisterUserForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
