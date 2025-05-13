b6d702c782b4bd0c767bcfc1db867ee68da3781f
simplyObare <josephatowiny@gmail.com>
2025-05-13 20:37:22 +0300

    - Create Path to Login Page in urls.py - Login Page Template - extend base.html and render the login form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+
+class Login(LoginView):
+    template_name = "users/accounts/login.html"
